78595844fcc2bccd43c36cc876a2466ad29748b6
Abdi Mussa <abdimussa87@gmail.com>
2023-11-30 22:40:34 +0300

    fix: made changes to unit test to use the dataframe getting returned from slack_parser

--- tests/test_slack_data_loader.py
+++ tests/test_slack_data_loader.py
@@ -14,10 +14,11 @@ class TestSlackDataLoader(unittest.TestCase):
         # Initialize SlackDataLoader with the path
         self.data_loader = SlackDataLoader("../anonymized")
 
-    def test_get_channels(self):
-        channels = self.data_loader.get_channels()
-        expected_columns = ['id', 'name', 'created','creator','is_archived','is_general','members','topic','purpose']
-        self.assertListEqual(list(channels.columns), expected_columns)
+    def test_slack_parser(self):
+        all_week_1_path = os.path.join("../anonymized", 'all-week1/')
+        week_1_df = self.data_loader.slack_parser(all_week_1_path)
+        expected_columns = ['msg_type', 'msg_content', 'sender_name','sender_name','sender_name','time_thread_start','reply_count','reply_users_count','reply_users','tm_thread_end','channel']
+        self.assertListEqual(list(week_1_df.columns), expected_columns)
 
    
 
