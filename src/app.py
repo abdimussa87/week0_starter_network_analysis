de1fd6b43f7bff3a10c9378b4ef58d1f0c7df6a7
Abdi Mussa <abdimussa87@gmail.com>
2023-12-01 19:24:10 +0300

    feat: working on streamlit to display top 20 message senders

--- src/app.py
+++ src/app.py
@@ -7,28 +7,42 @@ import seaborn as sns
 rpath = os.path.abspath('..')
 if rpath not in sys.path:
     sys.path.insert(0, rpath)
+import pprint
 
 from src.loader import SlackDataLoader
 import src.utils as utils
+import src.db_utils as db_utils
 
 # Initialize DataLoader
 data_loader = SlackDataLoader("../anonymized")
 all_week_1_path = os.path.join("../anonymized", 'all-week1/')
-week1_df = utils.slack_parser(all_week_1_path)
-week1_df.head()
+week1_df = data_loader.slack_parser(all_week_1_path)
 
-def get_top_20_user(data, channel='Random'):
+db = db_utils.DBWithSchema()
+channel_messages =  db.find_all('channel_messages')
+channel_messages_cleaned=[]
+
+for channel in channel_messages:
+    channel['_id']=str(channel.get('_id'))
+    channel_messages_cleaned.append(channel)
+
+
+
+def get_top_20_message_senders(data, channel='Random'):
     """get user with the highest number of message sent to any channel"""
 
-    data['sender_name'].value_counts()[:20].plot.bar(figsize=(15, 7.5))
-    plt.title(f'Top 20 Message Senders in #{channel} channels', size=15, fontweight='bold')
-    plt.xlabel("Sender Name", size=18); plt.ylabel("Frequency", size=14);
-    plt.xticks(size=12); plt.yticks(size=12);
-    plt.show()
+    top_20_senders = data['sender_name'].value_counts()[:20].to_frame(name='count').reset_index()
+    top_20_senders.columns = ['sender_name', 'message_count']
+    st.write(f'Top 20 Message Senders in #{channel} channel', size=15, fontweight='bold')
+    st.bar_chart(top_20_senders, x='sender_name', y='message_count', use_container_width=True)
+
+
+
 
 st.set_page_config(
-    page_title="ChatGPT",
-    page_icon=":robot:"
+    page_title="10Academy Slack Dashboard",
+    page_icon=":robot:",
+    layout="wide",
 )
 
 
@@ -38,4 +52,15 @@ st.subheader('Dashboard')
 with st.container():
     st.write('---')
     st.title('All week 1 Channel')
-    st.dataframe(week1_df)
+    st.write("This is the data loaded for the 'all-week-1' channel slack messages")
+    st.dataframe(channel_messages_cleaned)
+
+st.write('---')
+col_1 , col_2 = st.columns(2)
+with col_1:
+    
+    st.subheader('Top 20 Message Senders')
+    get_top_20_message_senders(week1_df, 'all-week-1')
+with col_2:
+    st.subheader('Bottom 20 Message Senders')
+    get_bottom_20_message_senders(week1_df, 'all-week-1')
