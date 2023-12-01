594ac1e68e4feafb59564ab2cce5b3bc54b662c3
Abdi Mussa <abdimussa87@gmail.com>
2023-12-01 19:24:31 +0300

    feat: showing bottom 20 message senders

--- src/app.py
+++ src/app.py
@@ -36,7 +36,13 @@ def get_top_20_message_senders(data, channel='Random'):
     st.write(f'Top 20 Message Senders in #{channel} channel', size=15, fontweight='bold')
     st.bar_chart(top_20_senders, x='sender_name', y='message_count', use_container_width=True)
 
+def get_bottom_20_message_senders(data, channel='Random'):
+    """get user with the lowest number of message sent to any channel"""
 
+    bottom_20_senders = data['sender_name'].value_counts()[-20:].to_frame(name='count').reset_index()
+    bottom_20_senders.columns = ['sender_name', 'message_count']
+    st.write(f'Bottom 20 Message Senders in #{channel} channel', size=15, fontweight='bold')
+    st.bar_chart(bottom_20_senders, x='sender_name', y='message_count', use_container_width=True)
 
 
 st.set_page_config(
